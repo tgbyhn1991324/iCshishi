4e1fe7af12902e86286243281b7f80ce12b4c6a8
陈伟 <896092886@>
2016-09-21 21:16:42 +0800

    修改

--- supcap/superid/key_key_table/user_attributes_table.py
+++
@@ -1,6 +0,0 @@
-#user_attributes
-IDM_USER_ID             = 0
-IDM_ATTRIBUTE_ID        = 1
-IDM_ATTRIBUTE_VALUE     = 2
-IDM_ORGANIZATION_ID     = 3
-IDM_PACKAGE_ID          = 4
--- supcap/tagFromMart/tm_tag/phone/tm_phone_tablepath.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-#for phone
-HDFS_PATH_PHONE_TAG_BASE_FROM_DOSS          = "/project/supcap/iinsight/tagFromMart/phone_tag_doss/key=1/"
-HDFS_PATH_PHONE_TAG_BASE_FROM_CAC_S_SRV_REQ = "/project/supcap/iinsight/tagFromMart/phone_tag/key=1/"
-
-#tm phone
-HDFS_PATH_PHONE_TAG                         = "/project/supcap/iinsight/tagFromMart/tm_tag/phone_tag/"
